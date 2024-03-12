3c754996705ec3fe8f4c9aa8880d6cd3187fbf03
Teja <coar530@gmail.com>
2024-03-12 17:17:28 +0530

    reducer hook

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import App13a from './App13a';
-import App15 from './App15';
-import App19 from './App19';
+
+import App21 from './App21';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <App19 />
+    <App21 />
   // {/* </React.StrictMode> */}
 );
 
